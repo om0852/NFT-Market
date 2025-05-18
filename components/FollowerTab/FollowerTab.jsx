decc561593afe9dc2a968b541bf5dcc649b08ef2
om salunke <omsaunke343@gmail.com>
2025-05-18 11:25:39 +0530

    FollowerTab done

--- components/FollowerTab/FollowerTab.jsx
+++ components/FollowerTab/FollowerTab.jsx
@@ -1,8 +1,91 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import {
+  RiUserFollowFill,
+  RiUserUnfollowFill,
+  RiAwardLine,
+} from "react-icons/ri";
 import Style from "./FollowerTab.module.css";
+import FollowerTabCard from "./FollowerTabCard/FollowerTabCard";
 const FollowerTab = () => {
-  return <div></div>;
+  const CardArray = [1, 2, 3, 4, 5];
+  const FollowingArray = new Array().fill(0);
+  const [popular, setPopular] = useState(true);
+  const [following, setFollowing] = useState(false);
+  const [news, setNews] = useState(false);
+const openPopular= ()=>{
+  if(!popular){
+    setPopular(true);
+    setNews(false);
+    setFollowing(false)
+  }
+}
+const openFollower= ()=>{
+  if(!following){
+    setPopular(false);
+    setNews(false);
+    setFollowing(true)
+  }
+}
+const openNews= ()=>{
+  if(!news){
+    setPopular(false);
+    setNews(true);
+    setFollowing(false)
+  }
+}
+
+  return (
+    <div className={Style.followerTab}>
+      <div className={Style.followerTab_title}>
+        <h2>Top Creators List...</h2>
+        <div className={Style.followerTab_tabs}>
+          <div className={Style.followerTab_tabs_btn}>
+            <button onClick={() => openPopular()}>
+              <RiUserFollowFill />
+              Popular
+            </button>
+            <button onClick={() => openFollower()}>
+              <RiUserUnfollowFill />
+              Following
+            </button>
+            <button onClick={() => openNews()}>
+              <RiAwardLine />
+              NoteWorthy
+            </button>
+          </div>
+        </div>
+      </div>
+      {popular && (
+        <div className={Style.followerTab_box}>
+          {CardArray.map((el, i) => (
+            <FollowerTabCard key={i + 1} i={i} el={el} />
+          ))}
+        </div>
+      )}
+      {following && (
+        <div className={Style.followerTab_box}>
+          {CardArray.map((el, i) =>( 
+            <FollowerTabCard key={i + 1} i={i} el={el} />
+          ))}
+        </div>
+      )}
+      {news && (
+        <div className={Style.followerTab_box}>
+          {CardArray.map((el, i) => {
+            return(
+              <FollowerTabCard key={i + 1} i={i} el={el} />
+            )
+          })}
+        </div>
+      )}
+      <div className={Style.followerTab_member}>
+        <div className={Style.followerTab_member_box}>
+          <a href="#">Show me more</a>
+          <a href="#">Become author</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FollowerTab;
---
+++ components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Style from "./FollowerTabCard.module.css";
+import { MdVerified } from "react-icons/md";
+import { TiTick } from "react-icons/ti";
+
+import images from "../../../img/index";
+const FollowerTabCard = ({ i }) => {
+  const [following, setFollowing] = useState(false);
+  const followMe = ()=>{
+    if(!following){
+        setFollowing(true)
+    }
+    else{
+        setFollowing(false)
+    }
+  }
+  return (
+    <div className={Style.followerTabCard}>
+      <div className={Style.followerTabCard_rank}>
+        <p>
+          #{i + 1}
+          <span>🥇</span>
+        </p>
+      </div>
+      <div className={Style.FollowerTabCard_box}>
+        <div className={Style.FollowerTabCard_box_img}>
+          <Image
+            src={images.creatorbackground1}
+            alt="image"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className={Style.followerTabCard_box_profile}>
+          <Image
+            className={Style.followerTabCard_box_profile_img}
+            src={images.creatorbackground1}
+            alt="profile bg"
+            width={500}
+            height={300}
+            objectFit={"cover"}
+          />
+        </div>
+        <div className={Style.followerTabCard_box_profile}>
+          <Image
+            className={Style.followerTabCard_box_profile_img}
+            src={images.user1}
+            alt="profile bg"
+            width={500}
+            height={300}
+            objectFit={"cover"}
+          />
+        </div>
+        <div className={Style.FollowerTabCard_box_info}>
+          <div className={Style.FollowerTabCard_box_info_name}>
+            <h4>
+              Giada Mann{" "}
+              <span>
+                <MdVerified />
+              </span>
+            </h4>
+            <p>12.321 ETH</p>
+          </div>
+          <div className={Style.FollowerTabCard_box_info_following}>
+            {following ? (
+              <a onClick={() => followMe()} >
+                Follow{" "}
+                <span>
+                  <TiTick />
+                </span>
+              </a>
+            ) : (
+              <a onClick={() => followMe()}> Following</a>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FollowerTabCard;
